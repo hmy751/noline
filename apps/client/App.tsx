782acac15300ca2ee278a0b896a401f41a92c707
함명연 <hmy751@gmail.com>
2025-10-11 22:37:06 +0900

    feat: repo/ui 패키지 tailwindcss, nativewind 추가 및 package 각 네이밍 수정

--- apps/client/App.tsx
+++ apps/client/App.tsx
@@ -1,20 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style='auto' />
+    <View className="flex-1 items-center justify-center bg-blue-500">
+      <Text className="text-white text-2xl font-bold">
+        Tailwind CSS is working!
+      </Text>
+      <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ apps/client/tailwind.config.js
@@ -0,0 +1,15 @@
+const path = require("path");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./App.tsx",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    path.resolve(__dirname, "../../packages/ui/src/**/*.{js,ts,jsx,tsx}"),
+  ],
+  presets: [require("@repo/ui/tailwind.config")],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
