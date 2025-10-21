28c9de62f6461e3d7f5d152037b08f3ff4c1085b
함명연 <hmy751@gmail.com>
2025-10-21 19:52:21 +0900

    feat: apps/client, env 추가

--- apps/client/babel.config.js
+++ apps/client/babel.config.js
@@ -3,6 +3,18 @@ module.exports = function (api) {
   return {
     presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }]],
     // metro 설정 충돌로 'nativewind/babel'은 설정하지 않음.
-    plugins: [],
+    plugins: [
+      [
+        'module:react-native-dotenv',
+        {
+          envName: 'APP_ENV',
+          moduleName: '@env',
+          path: '.env',
+          safe: false,
+          allowUndefined: true,
+          verbose: false,
+        },
+      ],
+    ],
   };
 };
--- apps/client/src/global.d.ts
+++ apps/client/src/global.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="nativewind/types" />
+
+declare module '@env' {
+  export const GEONAMES_API_URL: string;
+  export const GEONAMES_USERNAME: string;
+  export const EXPO_PUBLIC_API_URL: string;
+}
