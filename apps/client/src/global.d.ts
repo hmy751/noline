3e7086ff67d62eede318cb429cff5f8ec056afef
함명연 <hmy751@gmail.com>
2025-10-23 01:27:31 +0900

    chore: google map 관련 API 키 설정

--- apps/client/src/global.d.ts
+++ apps/client/src/global.d.ts
@@ -4,4 +4,6 @@ declare module '@env' {
   export const GEONAMES_API_URL: string;
   export const GEONAMES_USERNAME: string;
   export const EXPO_PUBLIC_API_URL: string;
+  export const EXPO_PUBLIC_GOOGLE_MAPS_IOS_API_KEY: string;
+  export const EXPO_PUBLIC_GOOGLE_MAPS_ANDROID_API_KEY: string;
 }
