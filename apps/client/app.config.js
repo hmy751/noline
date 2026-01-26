0a5ed7738afe5f44a16d2ae1b336c70ac4d697ed
함명연 <hmy751@gmail.com>
2026-01-26 19:35:16 +0900

    chore: 빋드번호 수정

--- apps/client/app.config.js
+++ apps/client/app.config.js
@@ -46,7 +46,7 @@ export default {
   ios: {
     supportsTablet: false,
     bundleIdentifier: IS_PROD ? 'com.ham.noline' : 'com.noline.dev',
-    buildNumber: '8',
+    buildNumber: '9',
     usesAppleSignIn: true,
     config: {
       googleMapsApiKey: process.env.EXPO_PUBLIC_GOOGLE_MAPS_IOS_API_KEY,
