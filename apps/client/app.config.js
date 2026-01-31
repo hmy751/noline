496f8b451ddf1d324e0b302b2a236eb09ee93b94
함명연 <hmy751@gmail.com>
2026-02-01 01:35:46 +0900

    chore: Render 서버로 마이그레이션, 버전 1.0.1

--- apps/client/app.config.js
+++ apps/client/app.config.js
@@ -4,7 +4,7 @@ export default {
   name: IS_PROD ? 'Noline' : 'Noline (Dev)',
   slug: 'noline',
   main: 'index.js',
-  version: '1.0.0',
+  version: '1.0.1',
   orientation: 'portrait',
   icon: './assets/icon.png',
   userInterfaceStyle: 'light',
